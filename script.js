cabe30dc9c42866e2e4be24a5c906600c2c3d3b5
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-01-26 15:33:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-var marker1 = L.marker([38.649, -90.261]).addTo(mymap);
-var marker2 = L.marker([38.640, -90.260]).addTo(mymap);
-var marker3 = L.marker([38.599, -90.237]).addTo(mymap);
-var marker4 = L.marker([38.646, -90.262]).addTo(mymap);
-var marker5 = L.marker([38.560, -90.252]).addTo(mymap);
+var marker1 = L.marker([38.649, -90.261]).addTo(viewDiv);
+var marker2 = L.marker([38.640, -90.260]).addTo(viewDiv);
+var marker3 = L.marker([38.599, -90.237]).addTo(viewDiv);
+var marker4 = L.marker([38.646, -90.262]).addTo(viewDiv);
+var marker5 = L.marker([38.560, -90.252]).addTo(viewDiv);
 
 marker1.bindPopup("<b>Resturant 1</b><br>Jeni's Splendid Ice Creams").openPopup();
 marker2.bindPopup("<b>Resturant 2</b><br>PokeDoke").openPopup();
