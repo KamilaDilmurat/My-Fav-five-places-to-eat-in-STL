4505490bbb65c2b91bc5b64de4190dba66ccba89
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-01-26 15:35:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+
+
+require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
+        const map = new Map({
+          basemap: "topo-vector"
+        });
+
+        const view = new MapView({
+          container: "viewDiv",
+          map: map,
+          zoom: 10, 
+          center: [38.640291685531224, -89.87012238264651] // longitude, latitude
+        });
+      });
+
+
+
 var marker1 = L.marker([38.649, -90.261]).addTo(viewDiv);
 var marker2 = L.marker([38.640, -90.260]).addTo(viewDiv);
 var marker3 = L.marker([38.599, -90.237]).addTo(viewDiv);
