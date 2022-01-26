cf7d6b7d55059c523c9ee9adb9b47dac0c12095b
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-01-26 15:53:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 
 
-require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
+require(["esri/Map", "esri/views/MapView"], (Map, STLfood) => {
         const map = new Map({
           basemap: "topo-vector"
         });
 
         const view = new MapView({
-          container: "viewDiv",
+          container: "STLfood",
           map: map,
-          zoom: 10, 
-          center: [38.640291685531224, -89.87012238264651] // longitude, latitude
+          zoom: 6, 
+          center: [38.640, -89.870] // longitude, latitude
         });
       });
 
 
 
-var marker1 = L.marker([38.649, -90.261]).addTo(viewDiv);
-var marker2 = L.marker([38.640, -90.260]).addTo(viewDiv);
-var marker3 = L.marker([38.599, -90.237]).addTo(viewDiv);
-var marker4 = L.marker([38.646, -90.262]).addTo(viewDiv);
-var marker5 = L.marker([38.560, -90.252]).addTo(viewDiv);
+var marker1 = L.marker([38.649, -90.261]).addTo(STLfood);
+var marker2 = L.marker([38.640, -90.260]).addTo(STLfood);
+var marker3 = L.marker([38.599, -90.237]).addTo(STLfood);
+var marker4 = L.marker([38.646, -90.262]).addTo(STLfood);
+var marker5 = L.marker([38.560, -90.252]).addTo(STLfood);
 
 marker1.bindPopup("<b>Resturant 1</b><br>Jeni's Splendid Ice Creams").openPopup();
 marker2.bindPopup("<b>Resturant 2</b><br>PokeDoke").openPopup();
